0112186ba9803a899a1938951ef206bad81b4e6c
ZaheerxBaloch <zaheerofficial005@gmail.com>
2025-08-05 17:42:19 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Replace this with your actual config from Firebase console
+const firebaseConfig = {
+    apiKey: "AIzaSyDlJrnwpE2FKlzv-5qtiLA2RjI4tq70aFg",
+    authDomain: "zenova-d69f5.firebaseapp.com",
+    projectId: "zenova-d69f5",
+    storageBucket: "zenova-d69f5.firebasestorage.app",
+    messagingSenderId: "964324915348",
+    appId: "1:964324915348:web:1e63f1c16efc1fac129c81"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
